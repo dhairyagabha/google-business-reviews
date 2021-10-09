bbf7fc529453b56ac985c67f1e26059af508f32d
Dhairya Gabhawala <gabhawaladhairya@gmail.com>
2021-10-08 22:23:53 -0500

    Initial commit

---
+++ lib/google/business/reviews.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "reviews/version"
+
+module Google
+  module Business
+    module Reviews
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
